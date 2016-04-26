4251b9bd65f887e787a7651fb08cb58f8e3209e4
박성호 <@Park-PC>
2016-04-26 20:19:19 +0900

    커어어어밋

--- Comma/src/main/java/comma/sist/controller/FileConfig.java
+++ Comma/src/main/java/comma/sist/controller/FileConfig.java
@@ -11,7 +11,7 @@ public class FileConfig {
 
 		
 		try{
-			String path = "C:\\Users\\ANBOYOUNG\\git\\P2_SecondProject\\Comma\\src\\main\\java\\";
+			String path = "C:\\Users\\¹Ú¼ºÈ£\\git\\P2_SecondProject\\Comma\\src\\main\\java\\";
 
 			path = path+pack.replace('.', '\\');
 			
